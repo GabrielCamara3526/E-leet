8034d3720614faf5a7723fd556996e5c0192a7e5
Gabriel Gomes Câmara <121787816+GabrielCamara3526@users.noreply.github.com>
2024-06-20 19:31:58 -0300

    Active button color
    Now, a button gets darker when you click it instead of flashing a white color.

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ main_label.pack()
 main_entry = Entry(root, font=("Helvetica", 18), width=35)
 main_entry.pack(padx=10, pady=10)
 
-submit_button = Button(root, font=('Arial', 14, 'bold'), text='Submit', width=14, bg="green", fg='white')
+submit_button = Button(root, font=('Arial', 14, 'bold'), text='Submit', width=14, bg="green", fg='white', activebackground='darkgreen', activeforeground='white')
 submit_button.pack()
 
 root.mainloop()
